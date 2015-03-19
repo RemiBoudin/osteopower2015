38e210e018e90e694d6addd26ab323acf16670d5
RemiBoudin <boyer.remi81@gmail.com>
2015-03-19 16:30:52 +0100

    Tools

--- Corba2015/src/MonAppliMessagerie/Tools.java
+++ Corba2015/src/MonAppliMessagerie/Tools.java
@@ -3,17 +3,35 @@
  */
 package MonAppliMessagerie;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author jeremy
  *
  */
 public class Tools {
-	public String convertName(String username, EntityName entity) {
-		switch (entity) {
-		
-		
-		}
-		
-		return username;
+	public static String convertNameToId(String username, EntityName entity) {
+			return (username+"_"+entity.toString());
+	}
+
+	public static String getDate() {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		return dateFormat.format(date);
+
+	}
+	
+	public static String genererSignature(String IOR){
+		return IOR;
+	}
+	
+	public String chiffrerMessage(String message, String publicKey){
+		return "[crypté]"+message;
+	}
+	
+	public String dechiffrerMessage(String message, String privateKey){
+		return message.substring(8);
 	}
 }
