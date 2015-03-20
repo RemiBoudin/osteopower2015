cc5fd081a92c2e0d10caf252097bca44d52ba488
RemiBoudin <boyer.remi81@gmail.com>
2015-03-20 15:10:58 +0100

    Ajout de commentaires et print de debug pour ACimpl et AEimpl

--- Corba2015/src/MonAppliMessagerie/ACimpl.java
+++ Corba2015/src/MonAppliMessagerie/ACimpl.java
@@ -2,90 +2,125 @@ package MonAppliMessagerie;
 
 import java.util.Hashtable;
 
-
-public class ACimpl extends ACPOA{
+public class ACimpl extends ACPOA {
 
 	private Certificat certificat;
-	private Hashtable<Integer,Certificat> listeCertificats; 
+	private Hashtable<Integer, Certificat> listeCertificats;
 	private String privateKey;
 	private String publicKey;
 	private String id;
 	private String idAVsrv;
 	private String idAEsrv;
-	
+
 	/**
 	 * Constructeur de la classe ACimpl
 	 */
-	public ACimpl(String username){
-		this.publicKey=(Tools.generateKeys(this.id))[0];
-		this.privateKey=(Tools.generateKeys(this.id))[1];
-		
-		this.id=Tools.convertNameToId(username,EntityName.AC_SERVER);
-		this.idAVsrv=Tools.convertNameToId(username,EntityName.AV_SERVER);
-		this.idAVsrv=Tools.convertNameToId(username,EntityName.AE_SERVER);
-		
-		this.certificat = new Certificat(this.id,null,(short) 1,Tools.getDate(),"never",this.publicKey,"",Tools.genererSignature(this.id));
-		
-		this.listeCertificats = new Hashtable<Integer,Certificat>();
+	public ACimpl(String username) {
+		this.publicKey = (Tools.generateKeys(this.id))[0];
+		this.privateKey = (Tools.generateKeys(this.id))[1];
+
+		this.id = Tools.convertNameToId(username, EntityName.AC_SERVER);
+		this.idAVsrv = Tools.convertNameToId(username, EntityName.AV_SERVER);
+		this.idAVsrv = Tools.convertNameToId(username, EntityName.AE_SERVER);
+
+		this.certificat = new Certificat(this.id, null, (short) 1,
+				Tools.getDate(), "never", this.publicKey, "",
+				Tools.genererSignature(this.id));
+
+		this.listeCertificats = new Hashtable<Integer, Certificat>();
 		this.listeCertificats.put(1, this.certificat);
 	}
-	
+
 	/**
 	 * Permet de générer un certificat
-	 * @param publicKey clé publique de l'entité propriétaire du certificat
-	 * @param id Nom de l'entité propriétaire du certificat
-	 * @param dateExp date d'expiration du certificat
-	 * @param date date de début de validité du certificat
-	 * @param usage type d'usage attendu pour ce certificat
+	 * 
+	 * @param publicKey
+	 *            clé publique de l'entité propriétaire du certificat
+	 * @param id
+	 *            Nom de l'entité propriétaire du certificat
+	 * @param dateExp
+	 *            date d'expiration du certificat
+	 * @param date
+	 *            date de début de validité du certificat
+	 * @param usage
+	 *            type d'usage attendu pour ce certificat
 	 * @return le certificat nouvellement créé
 	 */
-	private Certificat creerCertificat(String publicKey,String id, String dateExp, String date, String usage){
+	private Certificat creerCertificat(String publicKey, String pptaire,
+			String dateExp, String date, String usage) {
 		int nbCertificats = this.listeCertificats.size();
-		Certificat newCertif = new Certificat(id,this.idAVsrv,(short) (nbCertificats+1),date,dateExp,publicKey,usage,Tools.genererSignature(id));
+		Certificat newCertif = new Certificat(pptaire, this.idAVsrv,
+				(short) (nbCertificats + 1), date, dateExp, publicKey, usage,
+				Tools.genererSignature(id));
+
+		System.out.println(this.id + " - INFO - Certificat créé pour "
+				+ pptaire);
+
 		return newCertif;
 	}
-	
+
 	@Override
 	/**
 	 * renvoi le certificat de l'AC
 	 */
 	public Certificat getCertificat() {
 		// TODO Auto-generated method stub
+		System.out.println(this.id + " - INFO - Certificat personnel envoyé");
 		return this.certificat;
 	}
 
-
 	/**
 	 * Stocke un certificat dans la liste des certificats
-	 * @param newCertif certificat à stocker dans la liste
+	 * 
+	 * @param newCertif
+	 *            certificat à stocker dans la liste
 	 */
 	private void stockerCertificat(Certificat newCertif) {
 		// TODO Auto-generated method stub
-		int nbCertificats = this.listeCertificats.size();
-		this.listeCertificats.put(nbCertificats+1,newCertif);
+
+		this.listeCertificats.put((int) newCertif.Num_Unique, newCertif);
+		System.out.println(this.id + " - INFO - Certificat de "
+				+ newCertif.proprietaire + " stocké avec l'id "
+				+ newCertif.Num_Unique);
+
 	}
 
-
+	/**
+	 * Génère un certificat et le stocke dans la base, suite à une demande.
+	 */
 	@Override
 	public void enregistrer(String clePublique, String proprietaire,
 			String dateExpiration, String usage) {
 		// TODO Auto-generated method stub
-		
+
 		// Création du certificat
-		Certificat newCertif = this.creerCertificat(usage, clePublique, dateExpiration, Tools.getDate(), usage);
-		
+		Certificat newCertif = this.creerCertificat(usage, clePublique,
+				dateExpiration, Tools.getDate(), usage);
+
 		// Stockage du certificat dans la base de certificat de l'AC
 		this.stockerCertificat(newCertif);
-		
+
+		// Publier auprès de l'AE
+		System.out.println(this.id + " - INFO - " + proprietaire
+				+ " Publication du certificat auprès de l'AE");
+
 	}
 
+	/**
+	 * Revoque un certificat auprès de l'AE et fait le retour au porteur
+	 */
 	@Override
 	public void revoquerCertificat(Certificat certificatPorteur, String periode)
 			throws certif_revoque {
 		// revoquer certificat sur l'AV
-		
-				// en fonction du retour, faire le retour
-		
+		System.out.println(this.id + " - INFO - "
+				+ certificatPorteur.proprietaire
+				+ " Demande de révocation aurpès de l'AV");
+
+		// Faire le retour à l'AE
+		System.out.println(this.id + " - INFO - "
+				+ certificatPorteur.proprietaire
+				+ " Résultat de la révocation envoyée à l'AE");
 	}
 
 }
--- Corba2015/src/MonAppliMessagerie/AEimpl.java
+++ Corba2015/src/MonAppliMessagerie/AEimpl.java
@@ -10,7 +10,9 @@ public class AEimpl extends AEPOA {
 
 	/**
 	 * Constructeur de la classe AE
-	 * @param username nom à donner à l'AE
+	 * 
+	 * @param username
+	 *            nom à donner à l'AE
 	 */
 	public AEimpl(String username) {
 		this.id = Tools.convertNameToId(username, EntityName.AE_SERVER);
@@ -22,79 +24,115 @@ public class AEimpl extends AEPOA {
 
 	}
 
-
-
-
 	/**
 	 * 
 	 */
 	@Override
 	public void publier(Certificat certificatPorteur) {
-		// faire afficher "INFO - Enregistrement - Enregistrement OK" sur le client
+		// faire afficher "INFO - Enregistrement - Enregistrement OK" sur le
+		// client
+		System.out.println(this.id + " - INFO - Certificat de "
+				+ certificatPorteur.proprietaire + " publié !");
 
 	}
 
 	/**
 	 * Vérifie la combinaison identifiant/mot de passe dans la base
-	 * @param user nom de l'utilisateur
-	 * @param mdp mot de passe de l'utilisateur
+	 * 
+	 * @param user
+	 *            nom de l'utilisateur
+	 * @param mdp
+	 *            mot de passe de l'utilisateur
 	 * @return true si OK, false si non OK
 	 */
 	private boolean authentifier(String user, String mdp) {
 		if (this.listeIdClientPorteur.containsKey(user)) {
 			if (this.listeIdClientPorteur.get(user).equals(mdp)) {
+				System.out.println(this.id + " - INFO - " + user
+						+ " authentifié avec succès");
+
 				return true;
 			} else {
+				System.out.println(this.id + " - ERR - " + user
+						+ " echec d'authentification (mdp erroné)");
 				return false;
+
 			}
 		} else {
+			System.out.println(this.id + " - ERR - " + user
+					+ " echec d'authentification (username inconnu)");
+			return false;
+
+		}
+	}
+
+	/**
+	 * Vérifie si l'utilisateur a le droit d'utiliser le certificat pour un
+	 * usage donné
+	 * 
+	 * @param user
+	 *            nom de l'utilisateur
+	 * @param certifPorteur
+	 *            certificat du porteur lié à l'utilisateur
+	 * @param usage
+	 *            type d'utilisation du certificat
+	 * @return true si OK, false si non OK
+	 */
+	private boolean verifierDroits(String user, Certificat certifPorteur,
+			String usage) {
+		// si usage certif == usage donné
+		if (true) {
+			System.out.println(this.id + " - INFO - " + user + " droits OK");
+			return true;
+		} else {
+			System.out.println(this.id + " - ERR - " + user + " droits NOK");
 			return false;
 		}
 	}
 
 	/**
-	 * Vérifie si l'utilisateur a le droit d'utiliser le certificat pour un usage donné
-	 * @param user nom de l'utilisateur
-	 * @param certifPorteur certificat du porteur lié à l'utilisateur
-	 * @param usage type d'utilisation du certificat
-	 * @return true si OK, false si non OK
+	 * Authentifie l'utilisateur et envoi la demande d'enregistrement à l'AC
 	 */
-	private boolean verifierDroits(String user, Certificat certifPorteur, String usage) {
-		// si usage certif == usage donné
-		if (true) {
-			return true;	
-		} else {
-			return false;
-		}
-	}
-
 	@Override
 	public Certificat saveCertificat(String clepublique, String proprietaire,
 			String mdp, String dateExpiration, String usage)
 			throws erreur_authent {
 		// Envoi de la demande à l'AC supérieure
-				Certificat newCertif = null;
-				
-				if(!this.authentifier(proprietaire, mdp)){
-					// faire afficher "ERR - Enregistrement - Echec d'authentification"
-				} else {
-					// faire enregistrer sur l'AC distant
-				}
-				
-				
-				return null;
+		Certificat newCertif = null;
+
+		if (!this.authentifier(proprietaire, mdp)) {
+			// faire afficher "ERR - Enregistrement - Echec d'authentification"
+			System.out.println(this.id + " - ERR - " + proprietaire
+					+ " echec d'authentification (username inconnu)");
+		} else {
+			// faire enregistrer sur l'AC distant
+			System.out.println(this.id + " - INFO - " + proprietaire
+					+ " Enregistrement auprès de l'AC");
+		}
+
+		return null;
 	}
 
+	/**
+	 * Révoque un certificat auprès l'AC
+	 */
 	@Override
 	public void revoquer(Certificat certificatPorteur, String mdp,
 			String periode) {
 		// TODO Auto-generated method stub
-		if(!this.authentifier(certificatPorteur.proprietaire, mdp)){
+		if (!this.authentifier(certificatPorteur.proprietaire, mdp)) {
 			// faire afficher "ERR - Revocation - Echec d'authentification"
+			System.out.println(this.id + " - ERR - "
+					+ certificatPorteur.proprietaire
+					+ " echec d'authentification (username inconnu)");
+
 		} else {
 			// faire révoquer sur l'AC
+			System.out.println(this.id + " - INFO - "
+					+ certificatPorteur.proprietaire
+					+ " Demande de révocation envoyée à l'AC");
+
 		}
 	}
-	
-	
+
 }
