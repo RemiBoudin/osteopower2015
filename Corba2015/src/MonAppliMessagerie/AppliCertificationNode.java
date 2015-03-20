7f71774cb8f7435ad6cef3fefe0f4b866c6ef3a1
RemiBoudin <boyer.remi81@gmail.com>
2015-03-20 16:16:48 +0100

    Créa du fichier "AppliCertificationNode"

---
+++ Corba2015/src/MonAppliMessagerie/AppliCertificationNode.java
@@ -0,0 +1,38 @@
+package MonAppliMessagerie;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AppliCertificationNode {
+
+	public static void main(String[] args) {
+
+		// Saisie du nom du noeud courant
+        System.out.println("AppliNode - Quel est le nom du noeud à créer?");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String nodeName = null; 
+        try {
+        	nodeName = in.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        // initialisation de l'AC
+        AppliAC ac = new AppliAC(nodeName);
+        ac.initClient();
+        ac.initServer();
+		
+        // initialisation de l'AE
+        AppliAE ae = new AppliAE(nodeName);
+        ae.initClient();
+        ae.initServer();
+        
+        // initialisation de l'AV
+        AppliAV av = new AppliAV(nodeName);
+        av.initClient();
+        av.initServer();
+	}
+
+}
