9a711bdeb8366ed5e7903bbaba3621e259788c08
RemiBoudin <boyer.remi81@gmail.com>
2015-03-20 14:20:43 +0100

    Modif de l'AEimpl.

--- Corba2015/src/MonAppliMessagerie/AEimpl.java
+++ Corba2015/src/MonAppliMessagerie/AEimpl.java
@@ -8,6 +8,10 @@ public class AEimpl extends AEPOA {
 	private String idACsrv;
 	private String id;
 
+	/**
+	 * Constructeur de la classe AE
+	 * @param username nom à donner à l'AE
+	 */
 	public AEimpl(String username) {
 		this.id = Tools.convertNameToId(username, EntityName.AE_SERVER);
 		this.idACsrv = Tools.convertNameToId(username, EntityName.AC_SERVER);
@@ -18,30 +22,53 @@ public class AEimpl extends AEPOA {
 
 	}
 
+	/**
+	 * Permet de lancer la procédure de création d'un certificat, puis retourne ce nouveau certificat
+	 */
 	@Override
-	public Certificat saveCertificat(String clepublique, short id, String mdp,
+	public Certificat saveCertificat(String clepublique, short user, String mdp,
 			String dateExpiration, String usage) throws erreur_authent {
 		// Envoi de la demande à l'AC supérieure
+		Certificat newCertif = null;
+		
+		if(!this.authentifier(user, mdp)){
+			// faire afficher "ERR - Enregistrement - Echec d'authentification"
+		} else {
+			// faire enregistrer sur l'AC distant
+		}
+		
+		
 		return null;
 	}
 
+	/**
+	 * Permet de révoquer un certificat donné à la date d'effet souhaitée
+	 */
 	@Override
-	public void revoquer(Certificat certificatPorteur, short id, String mdp,
+	public void revoquer(Certificat certificatPorteur, short user, String mdp,
 			String periode) {
-
-		if (listeIdClientPorteur.containsKey(certificatPorteur.Num_Unique)
-				&& listeIdClientPorteur.get(certificatPorteur.Num_Unique) == mdp) {
-			listeIdClientPorteur.remove(certificatPorteur.Num_Unique);
+		if(!this.authentifier(user, mdp)){
+			// faire afficher "ERR - Revocation - Echec d'authentification"
+		} else {
+			// faire révoquer sur l'AC
 		}
-
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public void publier(Certificat certificatPorteur) {
-		// TODO Auto-generated method stub
+		// faire afficher "INFO - Enregistrement - Enregistrement OK" sur le client
 
 	}
 
+	/**
+	 * Vérifie la combinaison identifiant/mot de passe dans la base
+	 * @param user nom de l'utilisateur
+	 * @param mdp mot de passe de l'utilisateur
+	 * @return true si OK, false si non OK
+	 */
 	private boolean authentifier(String user, String mdp) {
 		if (this.listeIdClientPorteur.containsKey(user)) {
 			if (this.listeIdClientPorteur.get(user).equals(mdp)) {
@@ -54,10 +81,21 @@ public class AEimpl extends AEPOA {
 		}
 	}
 
+	/**
+	 * Vérifie si l'utilisateur a le droit d'utiliser le certificat pour un usage donné
+	 * @param user nom de l'utilisateur
+	 * @param certifPorteur certificat du porteur lié à l'utilisateur
+	 * @param usage type d'utilisation du certificat
+	 * @return true si OK, false si non OK
+	 */
 	private boolean verifierDroits(String user, Certificat certifPorteur, String usage) {
 		// si usage certif == usage donné
-		
-		return true;
+		if (true) {
+			return true;	
+		} else {
+			return false;
+		}
 	}
-
+	
+	
 }
