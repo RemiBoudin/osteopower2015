4939d6ac4bdd3dab489e56321c1360e01b60d3ce
Jérémy Vaquier <jeremy.vaquier@gmail.com>
2015-03-21 13:48:06 +0100

    Ajout interface graphique

--- Corba2015/src/MonAppliMessagerie/AppliChat.java
+++ Corba2015/src/MonAppliMessagerie/AppliChat.java
@@ -44,6 +44,43 @@ public class AppliChat {
 			porteur.initServer();
 			user.initServer();
 			
+			System.out.println("## Projet CORBA ##\n");
+			System.out.println("-> T'es qui bizu ?");
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	        String clientName = in.readLine();
+	        
+			System.out.println("-> Bonjour "+clientName+" et bienvenue dans ce magnifique projet CORBA");
+			System.out.println("-> [1] Envoyer un message");
+			System.out.println("-> [2] Enregistrer un nouveau certificat");
+			System.out.println("-> [3] Révoquer un certificat");
+			System.out.println("-> [4] Quitter");
+			System.out.println("-> Que veux-tu faire ?\n");
+
+	        String sChoix = in.readLine();
+			
+	        switch(Integer.parseInt(sChoix)) {
+	        	case 1: // Cas envoi de message
+	    			System.out.println("-> A qui voulez vous écrire ?");
+	        		in = new BufferedReader(new InputStreamReader(System.in));
+	    	        String receiverName = in.readLine();
+	    	        System.out.println("-> Ecrivez votre message :");
+	    	        in = new BufferedReader(new InputStreamReader(System.in));
+	    	        String message = in.readLine();
+	        		user.repondreToUser(message,receiverName);
+	        		break;
+	        	case 2: // Cas enregistrement de certificat
+	        		porteur.enregistrerCertificat(String usage, String dateExp, String nodeName);
+	        		break;
+	        	case 3: // Cas révoquer un certificat
+	        		
+	        		break;
+	        	case 4: // Cas quitter
+	        		
+	        		break;
+	        	default:
+	        }
+	        
+		
 
 		} catch (Exception e) {
 			e.printStackTrace();
